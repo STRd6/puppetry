71a52e203b26d3c73912a1857ef53d797a476155
Daniel X. Moore <yahivin@gmail.com>
2011-05-31 10:47:52 -0700

    Added correct mode for sudoers file

--- modules/sudoers/manifests/init.pp
+++ modules/sudoers/manifests/init.pp
@@ -6,6 +6,7 @@ class sudoers {
   file { "/etc/sudoers":
     owner   => "root",
     group   => "root",
+    mode => 440,
     source  => "puppet:///modules/sudoers/sudoers",
     require => Package["sudo"]
   }
